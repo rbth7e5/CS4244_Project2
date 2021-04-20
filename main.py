93c2f313970e153dfd3deaac6b3914b3380b621b
Caijie <caijie96@gmail.com>
2021-04-20 19:18:57 +0800

    Generate cnf and weight file

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import math
 
 F = cnfgen.CNF()
 params_to_weights = dict()
+ind_format = "i{var_name}"
+param_format = "q{param_name}"
 
 
 def read_evid(f_name):
@@ -28,6 +30,7 @@ def generate_binary_array(num):
             start = start - 1
         copy_list[start] = True
         return copy_list
+
     init = [False for _ in range(num)]
     result = [init]
     for i in range(int(math.pow(2, num) - 1)):
@@ -37,8 +40,6 @@ def generate_binary_array(num):
 
 
 def table_to_cnf(table):
-    ind_format = "i{var_name}"
-    param_format = "q{param_name}"
     num_variables = len(table[0])
     variables = table[0]
     weights = table[1]
@@ -48,7 +49,8 @@ def table_to_cnf(table):
         param_name = param_format.format(param_name=var)
         F.add_clause([(False, ind_name), (True, param_name)])
         F.add_clause([(True, ind_name), (False, param_name)])
-        params_to_weights[param_name] = weights[1]
+        params_to_weights[param_name] = float(weights[1])
+        params_to_weights[ind_name] = 1
     else:
         binary_array = generate_binary_array(num_variables - 1)
         last_var = variables[-1]
@@ -58,7 +60,39 @@ def table_to_cnf(table):
             indicators_arr = [(not val, ind_format.format(var_name=variables[i])) for i, val in enumerate(arr)]
             F.add_clause(indicators_arr + [(False, last_ind_name)] + [(True, param_name)])
             F.add_clause(indicators_arr + [(True, last_ind_name)] + [(False, param_name)])
-            params_to_weights[param_name] = weights[i*2+1]
+            params_to_weights[param_name] = float(weights[i*2+1])
+            params_to_weights[last_ind_name] = 1
+            for _, ind_name in indicators_arr:
+                params_to_weights[ind_name] = 1
+
+
+def evidence_to_cnf(evidence):
+    def val_to_bool(val):
+        if val == '0':
+            return False
+        else:
+            return True
+    for var in evidence:
+        F.add_clause([(val_to_bool(evidence[var]), ind_format.format(var_name=var))])
+
+
+def print_to_file(name, content):
+    with open(name, 'w') as f:
+        f.truncate()
+        f.write(content)
+
+
+def generate_weight_file():
+    content = []
+    num_variables = 0
+    for i, var in enumerate(F.variables()):
+        num_variables += 1
+        weight = params_to_weights[var]
+        index = i + 1
+        content.append("w " + str(index) + " " + str(weight) + " 0")
+        content.append("w " + "-" + str(index) + " " + "{:.3f}".format(1 - weight) + " 0")
+    content.insert(0, "p " + str(num_variables))
+    print_to_file("weights", "\n".join(content))
 
 
 def main():
@@ -69,6 +103,9 @@ def main():
     tables = model.get_tables()
     for table in tables:
         table_to_cnf(table)
+    evidence_to_cnf(evidence)
+    print_to_file("cnf", F.dimacs())
+    generate_weight_file()
 
 
 if __name__ == '__main__':
